c76448a723c3d63b9a6812bf9e5a7face6c8d14a
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-07 21:50:00 +0530

    add code for this functionality.

--- app/src/main/java/com/example/openthewebsiteapp/MainActivity.java
+++ app/src/main/java/com/example/openthewebsiteapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.openthewebsiteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e;
+    Button b;
+    WebView w;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e = findViewById(R.id.editTextTextPersonName);
+        b = findViewById(R.id.button);
+        w = findViewById(R.id.webView);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = e.getText().toString();
+                w.loadUrl(s);
+            }
+        });
     }
 }
